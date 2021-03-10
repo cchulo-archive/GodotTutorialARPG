826b28fb1f3c2e4b895033bdb6249706756feb5a
Carlos Chulo <cchulo.dev@gmail.com>
2021-03-10 03:15:11 -0800

    created a detection zone for player

--- src/Bat.gd
+++ src/Bat.gd
@@ -1,15 +1,40 @@
 extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://assets/Effects/EnemyDeathEffect.tscn")
-const FRICTION = 200
-const FORCE = 150
+
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+export var FORCE = 150
 
 var knockback = Vector2.ZERO
+var velocity = Vector2.ZERO
 onready var stats = $Stats
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var state = CHASE
 
 func _ready():
 	print(stats.max_health)
 	print(stats.health)
+	
+func _process(delta):
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			
+		WANDER:
+			pass
+		CHASE:
+			pass
+			
+func seek_player():
+	pass
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
