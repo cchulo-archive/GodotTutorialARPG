75f5f8054379e79f2d7de145a8fc63022f95ef75
Carlos Chulo <cchulo.dev@gmail.com>
2021-03-10 02:22:45 -0800

    designed damage system

--- src/Bat.gd
+++ src/Bat.gd
@@ -4,7 +4,11 @@ const FRICTION = 200
 const FORCE = 150
 
 var knockback = Vector2.ZERO
+onready var stats = $Stats
 
+func _ready():
+	print(stats.max_health)
+	print(stats.health)
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -12,4 +16,8 @@ func _physics_process(delta):
 
 func _on_Hurtbox_area_entered(area):
 	if area is SwordHitbox:
+		stats.health -= 1
 		knockback = area.knockback_vector * 150
+
+func _on_Stats_no_health():
+	queue_free()
