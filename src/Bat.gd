64458d63f2d9dcd53f4c45d0ec13f3ec54b3a10e
Carlos Chulo <cchulo.dev@gmail.com>
2021-03-10 01:38:35 -0800

    checking to see if a variable exists

--- src/Bat.gd
+++ src/Bat.gd
@@ -11,5 +11,5 @@ func _physics_process(delta):
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
-	if area.get("knockback_vector") != null:
+	if area.knockback_vector != null:
 		knockback = area.knockback_vector * 150
